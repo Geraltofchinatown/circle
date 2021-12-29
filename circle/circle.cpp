ef924f0de9dabd1c9937e292632111652e93e209
pbbbbb <417209402@qq.com>
2021-12-29 10:57:09 +0800

    version 1.0

---
+++ circle/circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "circle.h"
+int main() {
+
+    circle a (3);
+    std::cout << a.round() << std::endl;
+
+    return 0;
+}
---
+++ circle/circle.h
@@ -0,0 +1,22 @@
+//
+// Created by pbbbbb on 2021/12/29.
+//
+
+#ifndef CIRCLE_CIRCLE_H
+#define CIRCLE_CIRCLE_H
+const static double PI = 3.14;
+
+class circle{
+
+private:
+    const double r;
+
+public:
+    circle (const double re=0)
+            : r(re)
+    {}
+
+    double round() const {return 2 * PI * r;}
+};
+
+#endif //CIRCLE_CIRCLE_H
